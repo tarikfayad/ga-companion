75482144ef4f63daf39670ab3b508c5d91cc4e3b
Tarik Fayad <tarikfayad@protonmail.com>
2025-01-15 14:20:29 -0500

    Created a RadialButton struct.

---
+++ Grand Archive Companion/Components/RadialButton.swift
@@ -0,0 +1,14 @@
+//
+//  RadialButton.swift
+//  Grand Archive Companion
+//
+//  Created by Tarik Fayad on 1/15/25.
+//
+
+import SwiftUI
+
+struct RadialButton {
+    let label: String
+    let image: Image
+    let action: () -> Void
+}
