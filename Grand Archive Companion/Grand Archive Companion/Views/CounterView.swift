b30733156c8014b400a6880cd9058b5889a8883f
Tarik Fayad <tarikfayad@protonmail.com>
2025-01-20 16:51:33 -0500

    Added a *very* basic tutorial overlay to the main score tracking scene. Will eventually animate it but for the sake of getting people testing the app just going with this.

--- Grand Archive Companion/Grand Archive Companion/Views/CounterView.swift
+++ Grand Archive Companion/Grand Archive Companion/Views/CounterView.swift
@@ -12,7 +12,7 @@ struct CounterView: View {
     
     @Environment(\.presentationMode) var presentationMode
     @Environment(\.modelContext) private var modelContext
-    @State private var isFirstLaunch: Bool = false
+    @State private var isFirstLaunch: Bool = true
     
     @Query private var players: [Player] // Fetch all Player models
     
@@ -33,33 +33,77 @@ struct CounterView: View {
     }
     
     var body: some View {
-        
-        VStack {
-            if numberOfPlayers > 1 {
-                ZStack {
-                    VStack {
-                        ZStack {
+        ZStack {
+            VStack {
+                if numberOfPlayers > 1 {
+                    ZStack {
+                        VStack {
+                            ZStack {
+                                PlayerCounterView (
+                                    backgroundColor: .playerPink,
+                                    fontColor: .white,
+                                    player: $playerTwo,
+                                    isSinglePlayer: false,
+                                    isTopPlayer: true,
+                                    onIncrementUpdate: { count in
+                                        addDamage(to: &playerTwo, value: count)
+                                    },
+                                    onDecrementUpdate: { count in
+                                        addDamage(to: &playerTwo, value: count)
+                                    }
+                                )
+                                .rotationEffect(.degrees(180))
+                                .ignoresSafeArea(.all)
+                            }
                             PlayerCounterView (
-                                backgroundColor: .playerPink,
+                                backgroundColor: .playerBlue,
                                 fontColor: .white,
-                                player: $playerTwo,
+                                player: $playerOne,
                                 isSinglePlayer: false,
-                                isTopPlayer: true,
                                 onIncrementUpdate: { count in
-                                    addDamage(to: &playerTwo, value: count)
+                                    addDamage(to: &playerOne, value: count)
                                 },
                                 onDecrementUpdate: { count in
-                                    addDamage(to: &playerTwo, value: count)
+                                    addDamage(to: &playerOne, value: count)
                                 }
                             )
-                            .rotationEffect(.degrees(180))
-                            .ignoresSafeArea(.all)
                         }
+                        VStack {
+                            HStack {
+                                CircleButtonView(imageName: "magnifyingglass", tintColor: Color.black, padding: 15, buttonSize: 20){
+                                    navigateToCardSearchView = true
+                                }
+                                
+                                CircleButtonView(imageName: "arrow.counterclockwise.circle", tintColor: Color.black, padding: 10, buttonSize: 30){
+                                    navigateToHistoryView = true
+                                }
+                            }
+                        }
+                        
+                        HStack {
+                            VStack {
+                                Button(action: {
+                                    presentationMode.wrappedValue.dismiss() // Go back
+                                }) {
+                                    HStack {
+                                        Image(systemName: "arrow.backward") // Custom back icon
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 20, height: 20)
+                                    } .foregroundStyle(.white)
+                                }
+                                .padding([.leading, .top])
+                                Spacer()
+                            }
+                            Spacer()
+                        }
+                    }
+                } else {
+                    ZStack {
                         PlayerCounterView (
                             backgroundColor: .playerBlue,
                             fontColor: .white,
                             player: $playerOne,
-                            isSinglePlayer: false,
                             onIncrementUpdate: { count in
                                 addDamage(to: &playerOne, value: count)
                             },
@@ -67,98 +111,144 @@ struct CounterView: View {
                                 addDamage(to: &playerOne, value: count)
                             }
                         )
-                    }
-                    VStack {
-                        HStack {
-                            CircleButtonView(imageName: "magnifyingglass", tintColor: Color.black, padding: 15, buttonSize: 20){
-                                navigateToCardSearchView = true
-                            }
-                            
-                            CircleButtonView(imageName: "arrow.counterclockwise.circle", tintColor: Color.black, padding: 10, buttonSize: 30){
-                                navigateToHistoryView = true
-                            }
-                        }
-                    }
-                    
-                    HStack {
+                        .padding(.top, -20)
+                        
                         VStack {
-                            Button(action: {
-                                presentationMode.wrappedValue.dismiss() // Go back
-                            }) {
-                                HStack {
-                                    Image(systemName: "arrow.backward") // Custom back icon
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 20, height: 20)
-                                } .foregroundStyle(.white)
+                            HStack {
+                                CircleButtonView(imageName: "magnifyingglass", tintColor: Color.black, padding: 15, buttonSize: 20){
+                                    navigateToCardSearchView = true
+                                }
+                                CircleButtonView(imageName: "arrow.counterclockwise.circle", tintColor: Color.black, padding: 10, buttonSize: 30){
+                                    navigateToHistoryView = true
+                                }
                             }
-                            .padding([.leading, .top])
                             Spacer()
                         }
-                        Spacer()
-                    }
-                }
-            } else {
-                ZStack {
-                    PlayerCounterView (
-                        backgroundColor: .playerBlue,
-                        fontColor: .white,
-                        player: $playerOne,
-                        onIncrementUpdate: { count in
-                            addDamage(to: &playerOne, value: count)
-                        },
-                        onDecrementUpdate: { count in
-                            addDamage(to: &playerOne, value: count)
-                        }
-                    )
-                    .padding(.top, -20)
-                    
-                    VStack {
+                        
                         HStack {
-                            CircleButtonView(imageName: "magnifyingglass", tintColor: Color.black, padding: 15, buttonSize: 20){
-                                navigateToCardSearchView = true
+                            VStack {
+                                Button(action: {
+                                    presentationMode.wrappedValue.dismiss() // Go back
+                                }) {
+                                    HStack {
+                                        Image(systemName: "arrow.backward") // Custom back icon
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 20, height: 20)
+                                    } .foregroundStyle(.white)
+                                }
+                                .padding([.leading, .top])
+                                Spacer()
                             }
-                            CircleButtonView(imageName: "arrow.counterclockwise.circle", tintColor: Color.black, padding: 10, buttonSize: 30){
-                                navigateToHistoryView = true
-                            }
-                        }
-                        Spacer()
-                    }
-                    
-                    HStack {
-                        VStack {
-                            Button(action: {
-                                presentationMode.wrappedValue.dismiss() // Go back
-                            }) {
-                                HStack {
-                                    Image(systemName: "arrow.backward") // Custom back icon
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 20, height: 20)
-                                } .foregroundStyle(.white)
-                            }
-                            .padding([.leading, .top])
                             Spacer()
                         }
-                        Spacer()
                     }
                 }
             }
-        }
-        .onAppear {
-            checkFirstLaunch()
-            setupPlayers()
-        }
-        .toolbar(.hidden)
-        .navigationBarBackButtonHidden(true)
-        .navigationDestination(isPresented: $navigateToCardSearchView) {
-            CardSearchView()
-        }
-        .navigationDestination(isPresented: $navigateToHistoryView) {
-            if numberOfPlayers == 1 {
-                HistoryView(multiplayer: false, playerOneColor: .playerBlue, playerOneDamageHistory: playerOne.damageHistory)
-            } else {
-                HistoryView(multiplayer: true, playerOneColor: .playerBlue, playerTwoColor: .playerPink, playerOneDamageHistory: playerOne.damageHistory, playerTwoDamageHistory: playerTwo.damageHistory)
+            .onAppear {
+                checkFirstLaunch()
+                setupPlayers()
+            }
+            .toolbar(.hidden)
+            .navigationBarBackButtonHidden(true)
+            .navigationDestination(isPresented: $navigateToCardSearchView) {
+                CardSearchView()
+            }
+            .navigationDestination(isPresented: $navigateToHistoryView) {
+                if numberOfPlayers == 1 {
+                    HistoryView(multiplayer: false, playerOneColor: .playerBlue, playerOneDamageHistory: playerOne.damageHistory)
+                } else {
+                    HistoryView(multiplayer: true, playerOneColor: .playerBlue, playerTwoColor: .playerPink, playerOneDamageHistory: playerOne.damageHistory, playerTwoDamageHistory: playerTwo.damageHistory)
+                }
+            }
+            
+            if isFirstLaunch {
+                if numberOfPlayers == 1 {
+                    VStack {
+                        HStack {
+                            VStack {
+                                Image(systemName: "arrow.up.right")
+                                    .padding(.bottom, 5)
+                                Text("Card Search")
+                                    .font(.system(size: 20, weight: .bold))
+                            }
+                            .frame(width: 150, height: 75)
+                            .background(Color.black)
+                            Spacer()
+                                .frame(width: 40)
+                            VStack {
+                                Image(systemName: "arrow.up.backward")
+                                    .padding(.bottom, 5)
+                                Text("Damage History")
+                                    .font(.system(size: 20, weight: .bold))
+                            }
+                            .frame(width: 160, height: 75)
+                            .background(Color.black)
+                        }.padding(.top, 70)
+                        Spacer()
+                        VStack {
+                            Text("Token Menu")
+                                .font(.system(size: 20, weight: .bold))
+                            Text("Open to add a token counter.\nSingle Tap to increment a token.\nDouble tap to decrement it.\nLong press to remove it.")
+                                .frame(width: 250)
+                                .font(.system(size: 15))
+                                .multilineTextAlignment(.center)
+                                .padding(.bottom, 10)
+                            Image(systemName: "arrow.down")
+                        }
+                        .frame(width: 250, height: 150)
+                        .background(Color.black)
+                        .padding(.bottom, 100)
+                    }
+                    .frame(width: UIScreen.main.bounds.width)
+                    .foregroundStyle(.white)
+                    .contentShape(Rectangle())
+                    .background(Color.black.opacity(0.3))
+                    .onTapGesture {isFirstLaunch = false}
+                    .animation(.easeInOut(duration: 0.3), value: isFirstLaunch)
+                } else {
+                    VStack {
+                        Spacer()
+                        HStack {
+                            VStack {
+                                Text("Card Search")
+                                    .font(.system(size: 20, weight: .bold))
+                                Image(systemName: "arrow.down.right")
+                                    .padding(.top, 5)
+                            }
+                            .frame(width: 150, height: 75)
+                            .background(Color.black)
+                            Spacer()
+                                .frame(width: 40)
+                            VStack {
+                                Text("Damage History")
+                                    .font(.system(size: 20, weight: .bold))
+                                Image(systemName: "arrow.down.backward")
+                                    .padding(.top, 5)
+                            }
+                            .frame(width: 160, height: 75)
+                            .background(Color.black)
+                        }.padding(.top, 70)
+                        Spacer()
+                        VStack {
+                            Text("Token Menu")
+                                .font(.system(size: 20, weight: .bold))
+                            Text("Open to add a token counter.\nSingle Tap to increment a token.\nDouble tap to decrement it.\nLong press to remove it.")
+                                .font(.system(size: 15))
+                                .multilineTextAlignment(.center)
+                                .padding(.bottom, 10)
+                            Image(systemName: "arrow.down")
+                        }.frame(width: 250, height: 150)
+                        .background(Color.black)
+                        .padding(.bottom, 100)
+                    }
+                    .frame(width: UIScreen.main.bounds.width)
+                    .foregroundStyle(.white)
+                    .contentShape(Rectangle())
+                    .background(Color.black.opacity(0.3))
+                    .onTapGesture {isFirstLaunch = false}
+                    .animation(.easeInOut(duration: 0.3), value: isFirstLaunch)
+                }
             }
         }
     }
