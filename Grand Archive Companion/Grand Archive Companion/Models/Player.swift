52fb18f4a484cf26cdd8f07681977ddd4e36d275
Tarik Fayad <tarikfayad@protonmail.com>
2025-01-20 06:57:30 -0500

    Updated the player model.

--- Grand Archive Companion/Grand Archive Companion/Models/Player.swift
+++ Grand Archive Companion/Grand Archive Companion/Models/Player.swift
@@ -10,24 +10,26 @@ import SwiftData
 @Model
 class Player {
     var id: UUID = UUID() // Unique identifier
+    var index: Int
     var damage: Int
     var levelCounters: Int
     var preparationCounters: Int
     var lashCounters: Int
     var enlightenmentCounters: Int
     var floatingMemory: Int
-    var incrementCount: [Int]
-    var decrementCount: [Int]
+    var incrementHistory: [Int]
+    var decrementHistory: [Int]
     
-    init(id: UUID, damage: Int, levelCounters: Int, preparationCounters: Int, lashCounters: Int, enlightenmentCounters: Int, floatingMemory: Int, incrementCount: [Int], decrementCount: [Int]) {
+    init(id: UUID, index: Int, damage: Int, levelCounters: Int, preparationCounters: Int, lashCounters: Int, enlightenmentCounters: Int, floatingMemory: Int, incrementHistory: [Int], decrementHistory: [Int]) {
         self.id = id
+        self.index = index
         self.damage = damage
         self.levelCounters = levelCounters
         self.preparationCounters = preparationCounters
         self.lashCounters = lashCounters
         self.enlightenmentCounters = enlightenmentCounters
         self.floatingMemory = floatingMemory
-        self.incrementCount = incrementCount
-        self.decrementCount = decrementCount
+        self.incrementHistory = incrementHistory
+        self.decrementHistory = decrementHistory
     }
 }
