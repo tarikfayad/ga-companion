b78edfacb666e3248ac8ddb3ba02e2ab196a36fd
Tarik Fayad <tarikfayad@protonmail.com>
2025-01-20 18:35:21 -0500

    Bumped the minimum supported iOS version to 18. SwiftData is just breaking on iOS 17. I'll come back to fix it on a later date.

--- Grand Archive Companion/Grand Archive Companion/Grand_Archive_CompanionApp.swift
+++ Grand Archive Companion/Grand Archive Companion/Grand_Archive_CompanionApp.swift
@@ -13,15 +13,15 @@ struct Grand_Archive_CompanionApp: App {
     @Environment(\.modelContext) private var modelContext
     @Environment(\.scenePhase) private var scenePhase
     
-    @State private var playerOne: Player = Player(index: 1)
-    @State private var playerTwo: Player = Player(index: 2)
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .onChange(of: scenePhase) { newPhase in
                     if newPhase == .background {
-                        Player.save(players: [playerOne, playerTwo], context: modelContext)
+                        let fetchedPlayers = Player.load(context: modelContext)
+                            if !fetchedPlayers.isEmpty {
+                                Player.save(players: fetchedPlayers, context: modelContext)
+                            }
                     }
                 }
         }.modelContainer(for: [Player.self])
