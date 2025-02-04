dd83e7acdc46b4519663e6ebcc88ad93243bc5e2
Tarik Fayad <tarikfayad@protonmail.com>
2025-02-03 19:43:38 -0500

    Created a deck row view to display decks in a list.
    Basically a carbon copy of the CardRowView but honestly I feel like it'd be messy using the same file with a bunch of if statements. This is more code but it's cleaner imo.

---
+++ Grand Archive Companion/Grand Archive Companion/Components/Row Views/DeckRowView.swift
@@ -0,0 +1,52 @@
+//
+//  DeckRowView.swift
+//  Grand Archive Companion
+//
+//  Created by Tarik Fayad on 2/3/25.
+//
+
+import SwiftUI
+
+struct DeckRowView: View {
+    @State var deck: Deck
+    @Environment(\.modelContext) private var modelContext
+    
+    var body: some View {
+        ZStack {
+            HStack {
+                Image(deck.champions[0].imageName())
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 60, height: 60)
+                VStack {
+                    HStack {
+                        Text(deck.name)
+                        Spacer()
+                    }
+                    .font(.system(size: 16, weight: .bold))
+                    .foregroundStyle(.white)
+                    .padding(.bottom, 4)
+                    
+                    HStack {
+                        Text(deck.elements.map(\.rawValue).joined(separator: " / "))
+                        Text("•")
+                        Text(String(Deck.winRate(deck: deck, context: modelContext)) + "%")
+                            .foregroundStyle(.gray)
+                        Spacer()
+                    }
+                    .font(.system(size: 12))
+                    .foregroundStyle(.gray)
+                    .textCase(.uppercase)
+                }
+                Image(systemName: "chevron.right")
+                    .foregroundColor(.gray)
+            }.frame(minHeight: 60)
+        }.foregroundStyle(.black)
+    }
+}
+
+#Preview {
+    List(0..<5) { item in
+//        DeckRowView()
+    }
+}
